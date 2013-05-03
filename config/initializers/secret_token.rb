017982d54cdae1ae62f59ef5050bcdc342f7f17b
John Ivnaoff <john.ivanoff@gmail.com>
2013-05-03 10:28:54 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GasGuzzlerApp::Application.config.secret_key_base = '8c41cc3cdd2c5896813b125f67214b46b9072e5e026636f92ffc1a85bb1075577653ce863bc5c29b98f9d0e7d3e5880ba156a9f77cd6c25a2146917f36b51741'
